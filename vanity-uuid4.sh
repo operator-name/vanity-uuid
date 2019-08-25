9ae734fdf7242ae148e3a73943b63e3b253641cc
operator.name <operator.name@protonmail.com>
2019-08-25 12:57:55 +0100

    Add vanity-uuid4 generator script

---
+++ vanity-uuid4.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+uuid0=$(grep -E "^.{8}$" hexwords.txt | shuf -n 1)
+uuid1=$(grep -E "^.{4}$" hexwords.txt | shuf -n 1)
+uuid2=4$(grep -E "^.{3}$" hexwords.txt | shuf -n 1)
+uuid3=$(grep -E "^(a|b).{3}$" hexwords.txt | shuf -n 1)
+uuid4=$(grep -E "^.{12}$" hexwords.txt | shuf -n 1)
+uuid=$uuid0-$uuid1-$uuid2-$uuid3-$uuid4
+
+echo $uuid 
+echo $uuid | sed "s/o/0/g;s/i/1/g;s/l/1/g;s/s/5/g;s/t/7/g"
