3ab327693218a4aef92940951665096184d9a6f5
operator.name <operator.name@protonmail.com>
2019-09-04 10:56:02 +0000

    Fix regex to match only hexwords instead of partial matches

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ with import <nixpkgs> {};
 let
     # stdenv comes with sed tr grep
     hexwords = pkgs.writeShellScriptBin "hexwords" ''
-        cat english-words/words.txt | tr '[:upper:]' '[:lower:]' | grep -E "(for|o|i|l|s|t|g|a|b|c|d|e|f|0|1|2|3|4|5|6|7|8|9|-|')*"
+        cat english-words/words.txt | tr '[:upper:]' '[:lower:]' | grep -E "^(for|o|i|l|s|t|g|a|b|c|d|e|f|0|1|2|3|4|5|6|7|8|9|-|')*$"
     '';
 in stdenv.mkDerivation rec {
     name = "vanity-uuid";
